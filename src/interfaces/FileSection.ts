9d2bb16d2c4900feb246f9900614dd517c3ae16e
karanvalechamsft <karanvalecha@microsoft.com>
2024-09-16 23:45:09 +0530

    🛠️ src/extension.ts -> Removed excludeInvalidFiles wrap 🛠️ src/interfaces/FileSection.ts -> Removed start and end indices 🛠️ src/logic.ts -> Added format variable; refactored content & index logic

--- src/interfaces/FileSection.ts
+++ src/interfaces/FileSection.ts
@@ -3,8 +3,6 @@ export interface FileSection {
     filePath: string;        // Full path of the file
     section: string;         // Section name or identifier
     toBeAnalysed: boolean;   // Flag indicating if the section should be analysed
-    startIndex: number;      // Start index of the section
-    endIndex: number;        // End index of the section
     appendAtEnd: boolean;    // Flag indicating if new content should be appended at the end of the section
   }
   
