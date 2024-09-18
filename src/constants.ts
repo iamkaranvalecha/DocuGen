ac8388836d046169c51643d7792e7c85e1f34fd4
Amit K Khanchandani <amitsystem@live.com>
2024-09-18 23:20:22 +0530

    Constants file updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,67 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export const Constants = {
-    extensionName: 'DocuGen',
-    prefix: `### File: `,
-    suffix: `\n\n----\n`,
-    newLine: `\n`,
-    fileTitle: "### AI Generated Documentation using DocuGen\n----\n",
-    supportedExtensions: [".js", ".ts", ".json", ".html", ".css", ".less", ".scss", ".vue", ".jsx", ".tsx", ".py", ".ipynb", ".yaml", ".yml", ".java", ".xml", ".properties", ".c", ".cpp", ".h", "Makefile", ".cs", ".config", ".rb", ".gemspec", ".gemfile", ".rake", ".php", ".swift", ".plist", ".kt", ".m", ".bat", ".ps1", "Dockerfile", ".tf"],
-    excludedFolders: [".git", ".vscode", ".vs", "node_modules", "dist", "build", ".venv", "bin", "obj", ".config"]
-}
+  extensionName: "DocuGen",
+  prefix: `### File: `,
+  suffix: `\n\n----\n`,
+  newLine: `\n`,
+  fileTitle: "### AI Generated Documentation using DocuGen\n----\n",
+  supportedExtensions: [
+    ".js",
+    ".ts",
+    ".json",
+    ".html",
+    ".css",
+    ".less",
+    ".scss",
+    ".vue",
+    ".jsx",
+    ".tsx",
+    ".py",
+    ".ipynb",
+    ".yaml",
+    ".yml",
+    ".java",
+    ".xml",
+    ".properties",
+    ".c",
+    ".cpp",
+    ".h",
+    "Makefile",
+    ".cs",
+    ".config",
+    ".rb",
+    ".gemspec",
+    ".gemfile",
+    ".rake",
+    ".php",
+    ".swift",
+    ".plist",
+    ".kt",
+    ".m",
+    ".bat",
+    ".ps1",
+    "Dockerfile",
+    ".tf",
+  ],
+  excludedFolders: [
+    ".git",
+    ".github",
+    ".semversioner",
+    ".vscode",
+    ".vs",
+    "node_modules",
+    "dist",
+    "build",
+    ".venv",
+    "bin",
+    "obj",
+    ".config",
+  ],
+};
 
 export function Configuration() {
-    return vscode.workspace.getConfiguration(Constants.extensionName.toLowerCase());
+  return vscode.workspace.getConfiguration(
+    Constants.extensionName.toLowerCase()
+  );
 }
