39922fdc5d9561d61b736ebf746924c51c19942b
Karan Valecha <iamkaranvalecha@outlook.com>
2024-09-28 22:13:12 +0530

    🛠️ src/Core/docugen.ts -> updated imports and methods 🔴 src/Core/interfaces/FileSection.ts 🔴 src/Core/interfaces/IDocuGen.ts 🔴 src/Core/interfaces/IModelProvider.ts 🔴 src/Core/interfaces/ISecretProvider.ts 🛠️ src/Core/package.json -> added displayName, types

--- src/Core/providers/IDocuGenProvider.ts
+++ src/Core/providers/IDocuGenProvider.ts
@@ -1,3 +1,3 @@
-export interface IDocuGen{
+export interface IDocuGenProvider {
     scanRepository(workspaceFsPath: string, excludeItemsFilePaths: string[], excludeExtensionsFilePaths: string[], itemsToBeIncludedFilePaths: (string | undefined)[], documentFilePath: string): void;
 }
