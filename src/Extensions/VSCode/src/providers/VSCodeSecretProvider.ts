4fa3956a2f09d59847f94afe78c7540fd8d0f791
Amit K Khanchandani <amitsystem@live.com>
2024-09-29 01:12:33 +0530

    🚀 (VSCodeSecretProvider.ts): update the way secrets are retrieved from configuration by using a more specific configuration namespace based on the extension name

--- src/Extensions/VSCode/src/providers/VSCodeSecretProvider.ts
+++ src/Extensions/VSCode/src/providers/VSCodeSecretProvider.ts
@@ -1,10 +1,10 @@
 // VSCodeSecretProvider.ts
 import * as vscode from 'vscode';
-import { ISecretProvider } from 'docugen';
+import { ISecretProvider,Constants } from 'docugen';
 
 export class VSCodeSecretProvider implements ISecretProvider {
     async getSecret(key: string): Promise<string | undefined> {
-        return vscode.workspace.getConfiguration().get<string>(`yourExtension.secrets.${key}`);
+        return vscode.workspace.getConfiguration(Constants.extensionName.toLowerCase()).get(key);
     }
     getConfiguration(){
         return vscode.workspace.getConfiguration();
