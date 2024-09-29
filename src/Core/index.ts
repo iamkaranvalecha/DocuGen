4f8968223dd8a8a1541df8a56862a5e758990afa
karanvalechamsft <karanvalecha@microsoft.com>
2024-09-29 18:45:26 +0530

    🛠️ src/Core/index.ts -> Removed two exports.

--- src/Core/index.ts
+++ src/Core/index.ts
@@ -1,6 +1,4 @@
 export { DocuGen } from './docugen';
 export { Providers } from './providers';
 export { Constants } from './constants';
-// export {IDocuGenProvider} from './providers/IDocuGenProvider';
-// export {IModelProvider} from './providers/IModelProvider';
-export {ISecretProvider} from './providers/ISecretProvider';
+export { ISecretProvider } from './providers/ISecretProvider';
