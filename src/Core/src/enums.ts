198c72a0196e13e121266e3bdf70e1e673977400
karanvalechamsft <karanvalecha@microsoft.com>
2024-10-02 14:02:50 +0530

    🛠️ src/Core/src/enums.ts -> Added detailed comments to SettingEnums

--- src/Core/src/enums.ts
+++ src/Core/src/enums.ts
@@ -7,9 +7,9 @@ export enum Enums {
 }
 
 export enum SettingEnums {
-    DefaultDocumentFileName = "defaultDocumentFileName",
-    IncludedItems = "includedItems",
-    ExcludedItems = "excludedItems",
-    UncheckedItems = "uncheckedItems",
-    SupportedExtensions = "supportedExtensions"
+    DefaultDocumentFileName = "defaultDocumentFileName", // This setting allows user to define the default file name for the generated documentation
+    IncludedItems = "includedItems", // This is the list of valid files that needs documentation
+    ExcludedItems = "excludedItems", // This is the list of unsupported files by DocuGen that should be excluded from being detected
+    UncheckedItems = "uncheckedItems", // This is the list of valid files to be excluded from documentation being generated
+    SupportedExtensions = "supportedExtensions" // This is the list of supported file extensions by DocuGen
 }
