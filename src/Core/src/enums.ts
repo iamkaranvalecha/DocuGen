243ea7022fda8f51a4742919017827ecdce86451
iamkaranvalecha <iamkaranvalecha@outlook.com>
2024-10-06 16:45:15 +0530

    Here is the brief summary of the changes:
    🛠️ examples/DocuGen.md -> Expanded method breakdown in summary
    🛠️ examples/docugen.json -> Updated included, excluded items
    🛠️ src/Core/src/docugen.ts -> Added error handling for empty included files
    🛠️ src/Core/src/enums.ts -> Clarified enum comments for file inclusion/exclusion
    🛠️ src/Extensions/VSCode/src/extension.ts -> Improved file filtering functions

--- src/Core/src/enums.ts
+++ src/Core/src/enums.ts
@@ -8,8 +8,8 @@ export enum Enums {
 
 export enum SettingEnums {
     DefaultDocumentFileName = "defaultDocumentFileName", // This setting allows user to define the default file name for the generated documentation
-    IncludedItems = "includedItems", // This is the list of valid files that needs documentation
-    ExcludedItems = "excludedItems", // This is the list of unsupported files by DocuGen that should be excluded from being detected
-    UncheckedItems = "uncheckedItems", // This is the list of valid files to be excluded from documentation being generated
+    IncludedItems = "includedItems", // This is the list of valid files with supported extension that needs documentation
+    ExcludedItems = "excludedItems", // This is the list of user & system defined folders & files that should be excluded
+    UncheckedItems = "uncheckedItems", // This is the list of valid files with supported extension to be excluded from documentation being generated
     SupportedExtensions = "supportedExtensions" // This is the list of supported file extensions by DocuGen
 }
