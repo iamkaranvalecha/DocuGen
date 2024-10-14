94c577a74f8db11cbaa0e4a7b2ad5f8d5b8b8ffa
amitkhanchandani-gep <Amit.Khanchandani@gep.com>
2024-10-14 17:33:06 +0530

    #18 Automate documentation changes and Git interactions
    This commit introduces a new function `commitDocumentationChanges` that automates the process of updating documentation files and committing these changes to Git. It also includes helper functions to configure the Git author, add files to Git, and push changes to a pull request branch.
    
    Additionally, it includes functions to write and update configuration files.
    
    ## Why
    This automation simplifies the process of maintaining and updating documentation, ensuring consistency and reducing manual errors.
    
    ## What
    - Added `commitDocumentationChanges` function to handle file creation/update and Git operations.
    - Implemented `configureGitAuthor` to set the Git author globally.
    - Created `writeConfigFile` and `updateConfigFile` functions to manage configuration files.
    - Included `addToGit` and `pushToGit` functions for Git interactions.
    
    ## How
    The functions use GitHub Actions core and exec modules to interact with the file system and Git. The `github` module is used to add comments to the pull request.

---
+++ src/Extensions/GithubActions/src/providers/writefile.ts
@@ -0,0 +1,115 @@
+import * as core from '@actions/core'
+import * as io from '@actions/io'
+import * as fs from 'fs'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import { Enums, SectionConfig } from 'docugen'
+import { Constants } from './constant'
+import { readConfigFile } from './utilities'
+import path from 'path'
+
+export async function commitDocumentationChanges(
+  filePath: string,
+  content: string
+) {
+  try {
+    configureGitAuthor()
+
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+      // const currentContent = fs.readFileSync(filePath, 'utf-8')
+      await fs.promises.mkdir(path.dirname(filePath), { recursive: true })
+      // If the file already contains the desired content, skip the update
+      // if (currentContent === content) {
+      //   core.info('File already has the desired content. No changes needed.')
+      //   return
+      // }
+    }
+
+    // Write content to the file (overwrite or create if not exists)
+    fs.writeFileSync(filePath, content)
+    core.info(`File ${filePath} created/updated with content.`)
+
+    await addToGit(filePath)
+
+    await pushToGit()
+
+    // Optionally, add a comment to the PR
+    const token = core.getInput('github-token')
+    const octokit = github.getOctokit(token)
+    const { owner, repo } = github.context.repo
+    const prNumber = github.context.issue.number
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: prNumber,
+      body: Constants.commitMessage
+    })
+
+    core.info('PR comment added.')
+  } catch (error: any) {
+    core.setFailed(error.message)
+  }
+}
+
+async function configureGitAuthor() {
+  await exec.exec('git', [
+    'config',
+    '--global',
+    'user.email',
+    '"you@example.com"'
+  ])
+  await exec.exec('git', ['config', '--global', 'user.name', '"Docugen"'])
+}
+
+export function writeConfigFile(filePath: string, sections: SectionConfig[]) {
+  if (sections !== undefined && sections.length > 0) {
+    fs.writeFileSync(filePath, JSON.stringify(sections, null, 2), 'utf-8')
+    core.info(`File ${filePath} created/updated with content.`)
+    addToGit(filePath)
+  } else {
+    throw new Error('Sections are undefined')
+  }
+}
+
+export function updateConfigFile(filePath: string, section: SectionConfig) {
+  if (section !== undefined) {
+    if (fs.existsSync(filePath)) {
+      const existingSectionsWithoutVSCode = readConfigFile(filePath).filter(
+        x => x.name !== Enums.GitHubActions
+      )
+      existingSectionsWithoutVSCode.push(section)
+
+      writeConfigFile(filePath, existingSectionsWithoutVSCode)
+    } else {
+      writeConfigFile(filePath, [section])
+    }
+  } else {
+    throw new Error('Section is undefined')
+  }
+}
+
+async function addToGit(filePath: string) {
+  // Add the file to Git
+  await exec.exec('git', ['add', filePath])
+}
+
+async function pushToGit() {
+  // Commit the change
+  await exec.exec('git', ['commit', '-m', Constants.commitMessage])
+
+  // Get the PR branch from the GitHub Actions context
+  const prBranch = github.context.payload.pull_request?.head.ref
+
+  if (!prBranch) {
+    throw new Error('Pull request branch not found')
+  }
+
+  // Pull the latest changes from the remote branch to avoid conflicts
+  await exec.exec('git', ['pull', 'origin', prBranch, '--rebase'])
+
+  // Push to the pull request branch
+  await exec.exec('git', ['push', 'origin', `HEAD:${prBranch}`])
+  console.log(`Changes pushed to branch ${prBranch}.`)
+}
